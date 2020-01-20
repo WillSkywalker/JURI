a5a20143f1f4b177bfc78d11a3e3b3797c10cd26
Will Skywalker <cxbats@gmail.com>
2020-01-20 09:40:09 +0100

    index fix

--- web/__init__.py
+++ web/__init__.py
@@ -66,13 +66,11 @@ def index():
         mname = Model.query.filter_by(pred_type='JUDGMENTS').order_by(-Model.fscore).first().modelname
 
     preds = Prediction.query.filter_by(pred_type='JUDGMENTS', modelname=mname).\
-        filter(exists().where(Prediction.appno != Judgments.appno)).\
+        filter(~exists().where(Prediction.appno == Judgments.appno)).\
         order_by(-Prediction.kpdate).limit(5).all()
 
     appnos = [pred.appno for pred in preds]
-    to_be_preds = Judgments.query.filter(Judgments.appno.in_(appnos)).all()
-    for j in to_be_preds:
-        j.res = conclusion_simple(j.conclusion)
+    to_be_preds = Decisions.query.filter(Decisions.appno.in_(appnos)).all()
 
     rests = Judgments.query.filter(exists().where(Prediction.appno == Judgments.appno)).\
         order_by(-Judgments.kpdate).limit(5).all()
