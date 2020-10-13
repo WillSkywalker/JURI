1de51a07a3876db737d584411de294454519840d
Will Skywalker <cxbats@gmail.com>
2020-10-13 23:36:18 +0200

    weekl y error

--- model/weekly.py
+++ model/weekly.py
@@ -137,5 +137,4 @@ def generate_weekly_report(press, modelname):
 
 if __name__ == '__main__':
     # with app.app_context():
-    weekly_report('Balanced Naive Bayes all cases v3'+str(datetime.datetime.now())
-
+    weekly_report('Balanced Naive Bayes all cases v3'+str(datetime.datetime.now()))
